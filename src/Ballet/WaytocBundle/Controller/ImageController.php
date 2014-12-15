cf219054b9330b0c7032af57e1d374347fc5c475
Paulius <kaffau@gmail.com>
2014-12-16 00:50:35 +0200

    NEW: Uplad image init

---
+++ src/Ballet/WaytocBundle/Controller/ImageController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Ballet\WaytocBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Ballet\WaytocBundle\Entity\Image;
+
+class ImageController extends Controller
+{
+    public function uploadAction(Request $request)
+    {
+        $image = new Image();
+        $form = $this->createFormBuilder($image)
+            ->add('name')
+            ->add('description')
+            ->add('file')
+            ->add('upload', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $userId = $this->get('security.context')->getToken()->getUser()->getId();
+            $image->setUserid($userId);
+            $image->upload();
+
+            $em->persist($image);
+            $em->flush();
+        }
+
+        return $this->render(
+            'BalletWaytocBundle:Form:upload.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    public function showImagesAction()
+    {
+        $images = $this->getDoctrine()
+            ->getRepository('BalletWaytocBundle:Image')->findAll();
+
+        return $this->render('BalletWaytocBundle:Default:index.html.twig', array('image' => $images));
+    }
+
+    public function showImageAction($slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+
+        $image =  $em->getRepository('BalletWaytocBundle:Image')->findBy(array('picId' => $slug ));
+
+        $this->generateUrl('item', array('slug' => $slug ));
+
+        return $this->render('BalletWaytocBundle:Page:item.html.twig', array('img' => $image, 'slug' => $slug));
+    }
+}
---
+++ src/Ballet/WaytocBundle/Entity/Image.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace Ballet\WaytocBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * User
+ */
+class Image
+{
+    /*
+     * @var integer
+     */
+    public $id;
+    /*
+     * @var string
+     */
+    public $name;
+    /*
+     * @var string
+     */
+    public $path;
+    /*
+     * @var string
+     */
+    private $file;
+
+    /**
+     * @var integer
+     */
+    private $picid;
+
+    /**
+     * @var integer
+     */
+    private $userid;
+
+    /**
+     * @var integer
+     */
+    private $description;
+
+    /**
+     * @var integer
+     */
+    private $uploaddate;
+
+
+    public function __construct()
+    {
+//        $this->userid = 96;
+        $this->uploaddate = new \DateTime('now');;
+    }
+    /**
+     * Get picid
+     *
+     * @return integer 
+     */
+    public function getPicid()
+    {
+        return $this->picid;
+    }
+
+    /**
+     * Set userid
+     *
+     * @param integer $userid
+     * @return Image
+     */
+    public function setUserid($userid)
+    {
+        $this->userid = $userid;
+
+        return $this;
+    }
+
+    /**
+     * Get userid
+     *
+     * @return integer 
+     */
+    public function getUserid()
+    {
+        return $this->userid;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Image
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Image
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/'.$this->path;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/images';
+    }
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function preUpload()
+    {
+        if (null !== $this->file) {
+            // do whatever you want to generate a unique name
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->path = $filename.'.'.$this->file->guessExtension();
+        }
+    }
+
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @return string
+     */
+    public function setDescription($description)
+    {
+        return $this->description = $description;
+    }
+
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and then the
+        // target filename to move to
+        $this->getFile()->move(
+            $this->getUploadRootDir(),
+            $this->getFile()->getClientOriginalName()
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->path = $this->getFile()->getClientOriginalName();
+
+        // clean up the file property as you won't need it anymore
+        $this->file = null;
+    }
+}
