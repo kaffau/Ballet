3dd2f6bc9eec81b8c650c511c7bf7a40cd4f50b4
Kaffau <kaffau@gmail.com>
2015-03-22 00:17:02 +0200

    #NEW: FB connection

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Ballet\WaytocBundle\BalletWaytocBundle(),
             new Ballet\UserBundle\UserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
