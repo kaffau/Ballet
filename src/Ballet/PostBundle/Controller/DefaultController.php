95de71f6dcc817b9476a007bf56cebbd74083e96
Kaffau <kaffau@gmail.com>
2015-06-25 23:29:49 +0300

    #NEW: Voting init

--- src/Ballet/PostBundle/Controller/DefaultController.php
+++ src/Ballet/PostBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Ballet\PostBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Ballet\PostBundle\Entity\Image;
 
 class DefaultController extends Controller
@@ -18,23 +19,27 @@ class DefaultController extends Controller
         $images = $this->getDoctrine()
             ->getRepository('BalletPostBundle:Image')->findBy(array(),array('picId' => 'DESC'));
 
-        $em = $this->getDoctrine()->getManager();
-        $image = new Image();
-        $form = $this->createFormBuilder($image)
-            ->add('avrAge', 'text')
-            ->add('save', 'submit', array('label' => 'Vote'))
-            ->getForm();
+//        $em = $this->getDoctrine()->getManager();
+//        $image = new Image();
+//        foreach($images as $img) {
+//
+//        }
+//        $form = $this->createFormBuilder($image)
+//            ->add('avrAge', 'text')
+//            ->add('save', 'submit', array('label' => 'Vote'))
+//            ->getForm();
+//
+//        $form->handleRequest($request);
+//        if ($form->isValid()) {
+//            $userId = $this->get('security.context')->getToken()->getUser()->getId();
+//            $image->setUserid($userId);
+//            $image = $form->getData();
+//            $em->persist($image);
+//            $em->flush();
+//            return $this->redirect($this->generateUrl('front_page'));
+//        }
 
-        $form->handleRequest($request);
-        if ($form->isValid()) {
-            $image = $form->getData();
-            var_dump($image->getPicId());
-            $em->persist($image);
-            $em->flush();
-            return $this->redirect($this->generateUrl('front_page'));
-        }
-
-        return $this->render('BalletWaytocBundle:Default:index.html.twig', array('image' => $images, 'form' => $form->createView()));
+        return $this->render('BalletWaytocBundle:Default:index.html.twig', array('image' => $images));
     }
 
     public function showImageAction($slug, Request $request)
@@ -45,6 +50,22 @@ class DefaultController extends Controller
         $this->generateUrl('post_image', array('slug' => $slug));
 
         return $this->render('BalletPostBundle:Page:item.html.twig', array('slug' => $slug, 'image' => $image));
+    }
+
+    public function voteAction(Request $request, $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $image = $this->getDoctrine()->getRepository('BalletPostBundle:Image')->find($slug);
+
+
+        $ajaxAge = $request->request->get('age');
+        $previousAge = $image->getAvrAge();
+        $age = $previousAge + $ajaxAge;
+        $image->setAvrAge($age);
+        $em->persist($image);
+        $em->flush();
+        return $this->render('BalletPostBundle:Page:vote.html.twig', array('image' => $image));
+
     }
 
 }
--- src/Ballet/WaytocBundle/Resources/public/assets/js/app.js
+++ src/Ballet/WaytocBundle/Resources/public/assets/js/app.js
@@ -3,4 +3,29 @@ $container.masonry({
     itemSelector: '.item'
 });
 
-$(".spinner").spinner();
+var spinner = $(".spinner").spinner({min: 16, max: 60}).val(26);
+
+
+$(function() {
+    $(".btn-circle").on("click", function(e) {
+        e.preventDefault();
+        var caption = $(this).closest(".caption");
+        var ageValue = $(this).closest(".thumbnail").find('.spinner').spinner().val();
+        var data = {age : ageValue};
+        var $href = $(this).attr('href');
+
+        $.ajax({
+            type: "POST",
+            url: $href,
+            data: data,
+            success: function (data) {
+                caption.empty();
+                caption.append('<h3>Thank you!</h3>');
+            },
+
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert('Error : ' + errorThrown);
+            }
+        })
+    })
+});
